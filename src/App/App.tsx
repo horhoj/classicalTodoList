495a0b4100e4f3e8e1aaa8b5862258e0724670bb
Vasiliy Zadoya <cool29@yandex.ru>
2024-05-26 03:49:43 +0500

    init

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import styles from './App.module.scss';
+import { Button } from '~/ui/Button';
+
+export function App() {
+  return (
+    <div className={styles.App}>
+      <Button>test</Button>
+    </div>
+  );
+}
